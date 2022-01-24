880b8fe3e082b52cbafc5119a51a975e3f8c6eb9
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-24 18:09:49 -0300

    feat: adiciona return early nos métodos das classses

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ class ContaCorrente {
     agencia;
     _saldo = 0;
     sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo = this._saldo - valor
-            console.log(`O novo saldo é de ${this._saldo}`)
+        if (this._saldo <= valor) {
+            console.log('Não foi possivel realizar o saque')
             return
         }
-        console.log('Não foi possivel realizar o saque')
+        this._saldo = this._saldo - valor
+        console.log(`O novo saldo é de ${this._saldo}`)
     }
     depositar(valor) {
-        if (valor >= 0) {
-            this._saldo += valor
-            console.log(`Após o depósito o saldo é de: ${this._saldo}`)
+        if (valor <= 0) {
+            console.log('Não é possível fazer um depósito de valor negativo')
             return
         }
-        console.log('Não é possível fazer um depósito de valor negativo')
+        this._saldo += valor
+        console.log(`Após o depósito o saldo é de: ${this._saldo}`)
     }
 }
 
