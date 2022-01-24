bf22fbd084964d3b98bf8ab36acc86571c1e89dc
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-24 17:21:26 -0300

    feat: criaçao de uma classe de contanta corrente com métodos

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 class Cliente {
     nome;
     cpf;
+
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor
+            console.log(`O novo saldo é de ${this.saldo}`)
+            return
+        }
+        console.log('Não foi possivel realizar o saque')
+    }
 }
 
 const cliente1 = new Cliente()
+const contaCorrentCliente1 = new ContaCorrente()
 cliente1.nome = 'Felipe'
 cliente1.cpf = 11122233390
-cliente1.agencia = 1001
-cliente1.saldo = 0
+contaCorrentCliente1.agencia = 1001
+contaCorrentCliente1.saldo = 100
+contaCorrentCliente1.sacar(200)
+
 
 const cliente2 = new Cliente()
 cliente2.nome = "Carla"
 cliente2.cpf = 22233344456
-cliente2.agencia = 1001
-cliente2.saldo = 0
+
 
 console.log(cliente2, cliente1)
