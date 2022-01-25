a3734ad641124a7307f8ecc4c0f101cdfd901324
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-25 15:31:47 -0300

    feat: ajuste no index

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const contaCorrentCliente1 = new ContaCorrente();
 cliente1.nome = "Felipe";
 cliente1.cpf = 11122233390;
 contaCorrentCliente1.agencia = 1001;
-contaCorrentCliente1.depositar(1000);
-contaCorrentCliente1.sacar(200);
+contaCorrentCliente1.cliente = cliente1;
+contaCorrentCliente1.depositar(500);
 
 const cliente2 = new Cliente();
 cliente2.nome = "Carla";
 cliente2.cpf = 22233344456;
+const contaCliente2 = new ContaCorrente();
+contaCliente2.agencia = 1002;
+contaCliente2.cliente = cliente2;
 
-console.log(cliente2, cliente1);
+contaCorrentCliente1.transferir(200, contaCliente2);
+
+console.log(contaCliente2, contaCorrentCliente1);
